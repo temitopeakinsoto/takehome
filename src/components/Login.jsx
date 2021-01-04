f9b4670ea59f2337ba18af2bb7f15b0fe4f42dd7
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-04 23:37:43 +0100

    Add login form input fields

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from 'react-bootstrap/Button';
+
 
 export default function Login() {
   return (
-    <Container className="p-3">
-      <Jumbotron>
-          <p>This is a sample test</p>
-      </Jumbotron>
-      <div>
-        <div>Illustration Image</div>
-        <div>form here</div>
-      </div>
-      <div>
-        <div>Register</div>
-        <div>Social Icons</div>
-      </div>
-    </Container>
+  <div>     
+    <form>
+  <div className="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+     
+    </div>
   );
 }
