cd7708df3f4853b955e5765d3084bac1f71ffd15
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-12 02:07:47 +0100

    add register form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,35 +5,52 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import { Image } from "semantic-ui-react";
 import Button from "react-bootstrap/Button";
 
-export default function Login() {
+export default function Register() {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
   return (
     <Container
-      style={{ border: "1px solid red", height:"100vh"}}
+      style={{ border: "1px solid red", height: "100vh" }}
       className="p-5"
     >
       <Row>
-        <Col style={{ textAlign:"center"}}>
-          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkdMs4He6pZZhqw-3xVPfjcH6cchV1rCQbg&usqp=CAU"/>
+        <Col style={{ textAlign: "center" }}>
+          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkdMs4He6pZZhqw-3xVPfjcH6cchV1rCQbg&usqp=CAU" />
         </Col>
         <Col>
           <Jumbotron style={{ flexGrow: "1" }}>
             <h1 className="header">Welcome!</h1>
             <form onSubmit={handleSubmit}>
               <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="exampleInputEmail1">First Name</label>
                 <input
-                  type="email"
+                  type="text"
                   className="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
+                  placeholder="Enter First Name"
+                />
+              </div>
+              <div className="form-group">
+                <label for="exampleInputEmail2">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail2"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter Last Name"
+                />
+              </div>
+              <div className="form-group">
+                <label for="exampleInputEmail3">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail3"
+                  aria-describedby="emailHelp"
                   placeholder="Enter email"
                 />
-                <small id="emailHelp" className="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
               </div>
               <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
@@ -44,22 +61,12 @@ export default function Login() {
                   placeholder="Password"
                 />
               </div>
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label ml-3" for="exampleCheck1">
-                  Remember Me!
-                </label>
-              </div>
               <Button type="submit" className="btn btn-primary mt-4">
-                Login
+                Register
               </Button>
               <small id="emailHelp" className="form-text text-muted">
-                  Don't have an account yet? Please sign up <Link to="/register">here</Link>.
-                </small>
+                Already have an account? Please Login <Link to="/">here</Link>.
+              </small>
             </form>
           </Jumbotron>
         </Col>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
