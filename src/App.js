92e798c1ebfb0608234ad6bf287e725e79eb1657
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-15 11:44:33 +0100

    add route for dashboard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
-import { Route, Router } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 import "./App.css";
 
 const App = () => (
-  <>
+  <>    
     <Route exact path="/" component={Login} />
     <Route path="/register" component={Register} />
+    <Route path="/home" component={Dashboard} />
   </>
 );
 
