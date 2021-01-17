352a487b5edb135e98ff8970d7b60ad86e6fd72f
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-17 23:36:05 +0100

    style track.js component

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -2,19 +2,35 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
 export default function Track(props) {
-    const { likes, track } = props
+  const { likes, updateLikes, track } = props;
   return (
-    <Jumbotron>
+    <Jumbotron
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
       <h1>{track.primary_artist.name}</h1>
-      <audio id="Test_Audio" controls>
-        <source
-          src={`https://genius.p.rapidapi.com/artists/16775/songs/${track.id}`}
-          type="audio/ogg"
-        />
-      </audio>
-      <span>
-        <p>{likes} </p>
-      </span>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+          width: "60%"
+        }}
+      >
+        <audio id="test_audio" controls>
+          <source
+            src={`https://genius.p.rapidapi.com/artists/16775/songs/${track.id}`}
+            type="audio/ogg"
+          />
+        </audio>
+
+        <span>
+          <h3 onClick={updateLikes}>{likes} </h3>
+        </span>
+      </div>
     </Jumbotron>
   );
 }
