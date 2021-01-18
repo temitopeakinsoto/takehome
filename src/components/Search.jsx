7f121f3dcdd376fd80892b3b93a294f926e48bd6
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-18 01:20:11 +0100

    Inititialize search component's slice of state and event handler

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
 
 
-export default function Search() {
+export default function Search(props) {
+    const { songs, setSongs } = props;
+    const [searchString, setSearchString] = useState("");
+
+    const handleChange = (e) => {
+        setSearchString(e.target.value);
+        const filteredResult = songs.filter(song => song.primary_artist.name === searchString);
+        setSongs(filteredResult);
+    }
   return (
     <Form.Group>
-      <Form.Control
+        <p>{searchString}</p>
+      <Form.Control      
+        onChange={handleChange}
         type="text"
         size="lg"
         placeholder="Enter email"
