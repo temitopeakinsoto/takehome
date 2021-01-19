6b762744992ecc046cf35ff0ee28aab264e8d1e1
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-19 23:52:07 +0100

    re-design login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,107 +1,152 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import { Image } from "semantic-ui-react";
-import Button from "react-bootstrap/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-export default function Login(props) {
-  const [formValues, setFormValues] = useState({
-    email: "",
-    password: "",
-  });
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  const submitFormValues = async (formValues) => {
-    const res = await fetch("localhost://", {
-        method: "POST",
-        headers: {
-            'Content-type': 'application/json' 
-        },
-        body: JSON.stringify(formValues)
-    });
-    console.log(formValues);
-  };
+export default function SignIn() {
+  const classes = useStyles();
+  const [formValues, setFormValues] = useState({ email: "", password: "", rememberMe: false });
 
-  const handleName = (e) => {
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
     setFormValues({
       ...formValues,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   };
-  const handleSubmit = (e) => {
-    
-    e.preventDefault();
-    submitFormValues(formValues)
-      .then((res) => {
-        console.log("Form successfully submited");
-        props.history.push('/home')
-      })
-      .catch((err) => {
-        console.log("There was an error submitting this form", err);
-      });
+
+  const handleCheckboxChange = e => {
+    const checked = e.target.checked;
+    setFormValues({
+      ...formValues,
+      rememberMe: checked,
+    });
+  }
+
+  const submitFormData = async (formData) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formValues)
+    };
+
+    try {
+      const res = await fetch("", options);
+    } catch(e){
+      console.log("There was an error with this request", e)
+    }
   };
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    submitFormData(formValues).then(respond => {
+      console.log("form successfully submitted", formValues);
+    }).catch(e => {
+      console.log("There was an error with this process", e)
+    });
+  };
+
   return (
-    <Container
-      style={{ border: "1px solid red", height: "100vh" }}
-      className="p-5"
-    >
-      <Row>
-        <Col style={{ textAlign: "center" }}>
-          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkdMs4He6pZZhqw-3xVPfjcH6cchV1rCQbg&usqp=CAU" />
-        </Col>
-        <Col>
-          <Jumbotron style={{ flexGrow: "1" }}>
-            <h1 className="header">Welcome!</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input
-                  onChange={handleName}
-                  value={formValues.email}
-                  name="email"
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input
-                  onChange={handleName}
-                  value={formValues.password}
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                />
-              </div>
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label ml-3" for="exampleCheck1">
-                  Remember Me!
-                </label>
-              </div>
-              <Button type="submit" className="btn btn-primary mt-4">
-                Login
-              </Button>
-              <p id="emailHelp" className="form-text text-muted">
-                Don't have an account yet? Please sign up{" "}
-                <Link to="/register">here</Link>.
-              </p>
-            </form>
-          </Jumbotron>
-        </Col>
-      </Row>
+    <Container component="main" maxWidth="xs">
+      {formValues.email} {formValues.password} {formValues.rememberMe}
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form
+          onSubmit={handleFormSubmission}
+          className={classes.form}
+          noValidate
+        >
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            name="rememberMe"
+            onChange={handleCheckboxChange}
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
     </Container>
   );
 }
