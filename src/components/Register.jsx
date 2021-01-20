92a9a69f3fb623dc374c8d76bf176061a4c0afda
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-20 17:55:22 +0100

    refactor Register interface component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,126 +1,169 @@
+
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import { Image } from "semantic-ui-react";
-import Button from "react-bootstrap/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Register() {
-  const [formInputVals, setFormInputVals] = useState({
+  const classes = useStyles();
+  const [formValues, setFormValues] = useState({
     fname: "",
     lname: "",
     email: "",
     password: "",
   });
 
-  const submitFormValues = async (formValues) => {
-    const url = "";
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const submitFormData = async (formData) => {
     const options = {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "content-type": "application/json",
       },
-      body: JSON.stringify(formValues),
+      body: JSON.stringify(formData),
     };
 
     try {
-      const res = await fetch(url, options);
-      console.log(formValues);
-    } catch(e) {
-      
+      const res = await fetch("", options);
+    } catch (e) {
+      console.log("There was an error with this request", e);
     }
   };
 
-  const onFormInputChange = (e) => {
-    setFormInputVals({
-      ...formInputVals,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleSubmit = (e) => {
+  const handleFormSubmission = (e) => {
     e.preventDefault();
-    submitFormValues(formInputVals)
-      .then((res) => {
-        console.log("Registration successful!");
+    submitFormData(formValues)
+      .then((respond) => {
+        console.log("form successfully submitted", formValues);
       })
-      .catch((err) => {
-        console.log("There was an error!", err);
+      .catch((e) => {
+        console.log("There was an error with this process", e);
       });
   };
+
   return (
-    <Container
-      style={{ border: "1px solid red", height: "100vh" }}
-      className="p-5"
-    >
-      <Row>
-        <Col style={{ textAlign: "center" }}>
-          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkdMs4He6pZZhqw-3xVPfjcH6cchV1rCQbg&usqp=CAU" />
-        </Col>
-        <Col>
-          <Jumbotron style={{ flexGrow: "1" }}>
-            <h1 className="header">Welcome!</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label for="exampleInputEmail1">First Name</label>
-                <input
-                  onChange={onFormInputChange}
-                  value={formInputVals.fname}
-                  name="fname"
-                  type="text"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter First Name"
-                />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputEmail2">Last Name</label>
-                <input
-                  onChange={onFormInputChange}
-                  value={formInputVals.lname}
-                  name="lname"
-                  type="text"
-                  className="form-control"
-                  id="exampleInputEmail2"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter Last Name"
-                />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputEmail3">Email address</label>
-                <input
-                  onChange={onFormInputChange}
-                  value={formInputVals.email}
-                  name="email"
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail3"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input
-                  onChange={onFormInputChange}
-                  value={formInputVals.password}
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                />
-              </div>
-              <Button type="submit" className="btn btn-primary mt-4">
-                Register
-              </Button>
-              <p id="emailHelp" className="form-text text-muted">
-                Already have an account? Please Login <Link to="/">here</Link>.
-              </p>
-            </form>
-          </Jumbotron>
-        </Col>
-      </Row>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form
+          onSubmit={handleFormSubmission}
+          className={classes.form}
+          noValidate
+        >
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="fname"
+            label="First Name"
+            name="fname"
+            autoComplete="fname"
+            autoFocus
+          />
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="lname"
+            label="Last Name"
+            type="lname"
+            id="lname"
+            autoComplete="lname"
+          />
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            label="email"
+            type="email"
+            id="email"
+            autoComplete="email"
+          />
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/login" variant="body2">
+                {"Don't have an account? Please Login"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
     </Container>
   );
 }
