2432c13df13f5e657422c024e7d73d1dc16b6951
Temitope Akinsoto <takinsoto@gmail.com>
2021-01-21 14:15:04 +0100

    add reset password screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import ForgotPassword from "./components/ForgotPassword"
+import ResetPassword from "./components/ResetPassword"
 
 import "./App.css";
 
@@ -11,6 +13,9 @@ const App = () => (
     <Route exact path="/" component={Login} />
     <Route path="/register" component={Register} />
     <Route path="/home" component={Dashboard} />
+    <Route exact path="/forgotpassword" component={ForgotPassword} />
+    <Route exact path="/resetPassword" component={ResetPassword} />
+
   </>
 );
 
---
+++ src/components/ResetPassword.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function ForgotPassword() {
+  const classes = useStyles();
+  const [formValues, setFormValues] = useState({ password: "", password2: "" });
+
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const submitFormData = async (formData) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    try {
+      const res = await fetch("", options);
+    } catch (e) {
+      console.log("There was an error with this request", e);
+    }
+  };
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    submitFormData(formValues)
+      .then((respond) => {
+        console.log("form successfully submitted", formValues);
+      })
+      .catch((e) => {
+        console.log("There was an error with this process", e);
+      });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Reset Your Password
+        </Typography>
+        <form
+          onSubmit={handleFormSubmission}
+          className={classes.form}
+          noValidate
+        >
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Enter Password"
+            name="password"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={handleInputChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Confirm Password"
+            name="password2"
+            autoComplete="email"
+            autoFocus
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
